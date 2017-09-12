76e0f200b9cedbb11f713457b02c86f3f7fa4678
sushildangi <dangi.sushil5@gmail.com>
2017-09-13 00:58:20 +0530

    CSS and Layout

--- src/main/java/com/isolutions4u/retailbanking/controllers/BankController.java
+++ src/main/java/com/isolutions4u/retailbanking/controllers/BankController.java
@@ -2,8 +2,12 @@ package com.isolutions4u.retailbanking.controllers;
 
 
 import com.isolutions4u.retailbanking.model.Branch;
+import com.isolutions4u.retailbanking.model.User;
 import com.isolutions4u.retailbanking.service.BranchService;
+import com.isolutions4u.retailbanking.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,20 +24,30 @@ public class BankController {
     @Autowired
     private BranchService branchService;
 
+    @Autowired
+    private UserService userService;
 
     @GetMapping(value = "/branchForm")
     public ModelAndView bankCreateForm() {
         ModelAndView modelAndView = new ModelAndView();
         Branch branch = new Branch();
+        userInfo(modelAndView);
         modelAndView.addObject("branch", branch);
         modelAndView.setViewName("admin/branch-creation");
         return modelAndView;
+    }
+
+    private void userInfo(ModelAndView modelAndView) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
     }
 
 
     @PostMapping(value = "/branchForm")
     public ModelAndView createNewUser(@Valid Branch branch, BindingResult bindingResult) {
         ModelAndView modelAndView = new ModelAndView();
+        userInfo(modelAndView);
         Branch branchExists = branchService.findBranchByBranchNo(branch.getBranchNo());
         if (branchExists != null) {
             bindingResult
@@ -44,14 +58,14 @@ public class BankController {
             modelAndView.setViewName("admin/branch-creation");
         } else {
             branchService.saveBranch(branch);
-            modelAndView.addObject("successMessage", "Branch has been created successfully");
+            String branchNo = branch.getBranchNo();
+            modelAndView.addObject("successMessage", "Branch created successfully and Branch code :" + branchNo);
             modelAndView.addObject("branch", new Branch());
             modelAndView.setViewName("admin/branch-creation");
 
         }
         return modelAndView;
     }
-
 
 
 }
--- src/main/java/com/isolutions4u/retailbanking/controllers/LoginController.java
+++ src/main/java/com/isolutions4u/retailbanking/controllers/LoginController.java
@@ -89,7 +89,6 @@ public class LoginController implements ErrorController {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = userService.findUserByEmail(auth.getName());
         modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
-        modelAndView.addObject("adminMessage", "Content Available Only for Users with Admin Role");
         modelAndView.setViewName("admin/index");
         return modelAndView;
     }

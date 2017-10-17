703f9c5b621482a5f17ddd41d670b6583767c013
sushildangi <dangi.sushil5@gmail.com>
2017-10-18 02:08:55 +0530

    Complete Crud Operation on Branch with RestController

--- src/main/java/com/isolutions4u/retailbanking/configurations/WebMvcConfig.java
+++ src/main/java/com/isolutions4u/retailbanking/configurations/WebMvcConfig.java
@@ -3,7 +3,10 @@ package com.isolutions4u.retailbanking.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
@@ -13,5 +16,6 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder;
     }
+
 
 }
